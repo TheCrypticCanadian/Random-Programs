dcf0d200e585c43b71a092f15105a0dddd783c91
Ethan Crann <ecrann@protonmail.com>
2020-05-22 18:24:51 -0300

    Added a string splitter

---
+++ Python/String-Spliter.py
@@ -0,0 +1,8 @@
+split_val = input("Val to split: ")
+print()
+user_input = input("Enter String: ")
+print()
+while user_input != "exit":
+    print(user_input.split(split_val))
+    user_input = input("Enter String: ")
+    print()
